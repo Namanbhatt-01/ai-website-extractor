f8e28d11ca3316040c46421e952c15dee84b8549
Namanbhatt-01 <the.namanbhatt109@gmail.com>
2026-04-18 21:07:47 +0530

    Add flaky bug hunter test case

--- sum.test.ts
+++ sum.test.ts
@@ -1,5 +1,4 @@
-import { expect, test } from 'vitest'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3)
-})
+test('flaky bug hunter', () => {
+  // Fails 50% of the time
+  expect(Math.random()).toBeGreaterThan(0.5);
+});
